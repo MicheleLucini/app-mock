4f6e35bbde0a7ed2566c6301b42320058e96664a
Michele Lucini <mic.lucini@gmail.com>
2021-10-21 00:20:01 +0200

    Switch scene

--- main.js
+++ main.js
@@ -2,10 +2,20 @@ var $headerIcons = document.getElementsByClassName("icon");
 var $main = document.getElementsByTagName("main")[0];
 var $footer = document.getElementsByTagName("footer")[0];
 var $bottomNav = document.getElementById("bottom_nav");
+var $btnTimbrature = document.getElementById("btnTimbrature");
+var $btnRichieste = document.getElementById("btnRichieste");
+var $btnDocumenti = document.getElementById("btnDocumenti");
 
 function clearContent() {
   $main.innerHTML = "";
   $footer.innerHTML = "";
+}
+
+function changeActiveBtnBottom(btnToActivate) {
+  $btnTimbrature.classList.remove("active");
+  $btnRichieste.classList.remove("active");
+  $btnDocumenti.classList.remove("active");
+  btnToActivate.classList.add("active");
 }
 
 function goToLogin() {
@@ -28,19 +38,30 @@ function goToInformativa() {
   $main.innerHTML += checkBox("Ho letto e accetto i termini e condizioni di utilizzo.");
   $main.innerHTML += checkBox("Ho letto e accetto l'informativa sulla privacy.");
   $main.innerHTML += checkBox("Ho letto e accetto l'utilizzo dei cookie come specificato nella cookie policy.");
-  $main.innerHTML += button('Prosegui', 'goToHome()');
+  $main.innerHTML += button('Prosegui', 'goToTimbrature()');
 }
 
-function goToHome() {
+function goToTimbrature() {
   clearContent();
   for (var i = $headerIcons.length; i--;) {
     $headerIcons[i].style.display = "grid";
   }
   $main.innerHTML += title('Timbrature', 'Queste sono le tue timbrature');
-  // $main.innerHTML += button('Logout', 'goToLogin()');
   $bottomNav.style.display = "grid";
+  changeActiveBtnBottom($btnTimbrature);
 }
 
+function goToRichieste() {
+  clearContent();
+  $main.innerHTML += title('Richieste', 'Ecco le tue richieste di ferie e permessi');
+  changeActiveBtnBottom($btnRichieste);
+}
+
+function goToDocumenti() {
+  clearContent();
+  $main.innerHTML += title('Documenti', 'Qui raccogliamo tutti i tuoi documenti');
+  changeActiveBtnBottom($btnDocumenti);
+}
 
 function title(title, subtitle) {
   return '<div class="title"><h1>' + title + '</h1><span>' + subtitle + '</span></div>';
